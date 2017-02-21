f91c864abece704c817d38e29f15135116d05977
Keomony Khun <keomony@gmail.com>
2017-02-21 13:13:59 +0000

    Use subject(:airport) for refactor

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,12 @@
 describe Airport do
+  subject(:airport) { described_class.new }
 
   it "should respond to land method with an argument" do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(airport).to respond_to(:land).with(1).argument
   end
 
   it "should respond to take_off method" do
-    expect(subject).to respond_to(:take_off).with(1).argument
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 
 end
