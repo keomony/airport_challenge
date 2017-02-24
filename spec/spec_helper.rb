536a7546d5112999e095f30455f87ab30d06183e
Keomony Khun <keomony@gmail.com>
2017-02-24 12:34:52 +0000

    Apply SRP for stormy solution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require './lib/airport'
 require './lib/plane'
+require './lib/weather_reporter'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
---
+++ spec/weather_reporter_spec.rb
@@ -0,0 +1,11 @@
+describe WeatherReporter do
+  subject(:weather_reporter) { described_class.new }
+  describe '#stormy' do
+    it 'can be non_stormy' do
+      expect(weather_reporter.stormy?).to be false
+    end
+    it 'can be stormy' do
+      expect(weather_reporter.stormy?).to be true
+    end
+  end
+end
