2e7fd0ea7df39da26720758a29c5fbd9ee72246b
Keomony Khun <keomony@gmail.com>
2017-02-24 12:39:13 +0000

    Mock Kernel.rand to return the wanted values

--- spec/weather_reporter_spec.rb
+++ spec/weather_reporter_spec.rb
@@ -2,9 +2,11 @@ describe WeatherReporter do
   subject(:weather_reporter) { described_class.new }
   describe '#stormy' do
     it 'can be non_stormy' do
+      allow(Kernel).to receive(:rand).and_return 1
       expect(weather_reporter.stormy?).to be false
     end
     it 'can be stormy' do
+      allow(Kernel).to receive(:rand).and_return 6
       expect(weather_reporter.stormy?).to be true
     end
   end
