5d5cd6acff1364d4204cfb525843b0ff2dd18d46
Keomony Khun <keomony@gmail.com>
2017-02-25 16:18:34 +0000

    Refoctor class Plane by removing instance variable to private

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,18 +5,21 @@ class Plane
   end
 
   def take_off
-    raise "Plane cann't take off: plane already flying" if @flying
+    raise "Plane cann't take off: plane already flying" if flying
   end
 
   def land(airport)
-    raise "Plane cann't land: plane already landed" unless @flying
+    raise "Plane cann't land: plane already landed" unless flying
     @flying = false
     @airport = airport
   end
 
   def airport
-    raise "Plane cann't be at an airport: plane already flying" if @flying
+    raise "Plane cann't be at an airport: plane already flying" if flying
     @airport
   end
+
+  private
+  attr_reader :flying
 
 end
